658c976a89070b9f50fb61c898acfd7ae5cbe762
lucasweiby <lucas_weiby@hotmail.com>
2019-06-05 10:16:52 -0300

    First commit (again lol)

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GO! Branding + Design</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><img src="assets/icons/bars.svg"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="#"><img src="assets/icons/search.svg"></span></a>
+                    <a class="nav-item nav-link" href="#">Home</span></a>
+                    <a class="nav-item nav-link" href="#">Quem somos</a>
+                    <a class="nav-item nav-link" href="#">O que fizemos</a>
+                    <a class="nav-item nav-link" href="#">Onde estamos</a>
+                    <a class="nav-item nav-link" href="#">Fale conosco</a>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ views/cases.php
@@ -0,0 +1,50 @@
+<section class="cases">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 case case-1">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+            <div class="col-md-6 case case-2">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+            <div class="col-md-12 case case-3">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 case case-1">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+            <div class="col-md-6 case case-2">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+            <div class="col-md-12 case case-3">
+                <div class="overlay">
+                    <small class="type">Case</small>
+                    <h1 class="case-title">Lorem ipsum</h1>
+                    <p class="category">Identidade visual</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
