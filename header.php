dc945af517b41ede58d0c1aa2c6484185375cc3f
lucasweiby <lucas_weiby@hotmail.com>
2019-06-05 18:31:07 -0300

    Página quem somos

--- header.php
+++ header.php
@@ -7,16 +7,15 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="index.php">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"><img src="assets/icons/bars.svg"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link search-item" href="#"><img class="search" src="assets/icons/search.svg"></span></a>
-                    <a class="nav-item nav-link" href="index.php">Home</span></a>
                     <a class="nav-item nav-link" href="who.php">Quem somos</a>
-                    <a class="nav-item nav-link" href="#">O que fizemos</a>
+                    <a class="nav-item nav-link" href="index.php">O que fizemos</a>
                     <a class="nav-item nav-link" href="#">Onde estamos</a>
                     <a class="nav-item nav-link" href="#">Fale conosco</a>
                 </div>
